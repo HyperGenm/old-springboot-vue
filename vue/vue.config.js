a10633ebc17d6ce740747d940b244ec1f14432d3
weiziplus <1602802727@qq.com>
2019-08-02 16:45:14 +0800

    1.1.0.  css文件打包为一个
    Signed-off-by: weiziplus <1602802727@qq.com>

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     productionSourceMap: false,
     css: {
         // 是否使用css分离插件 ExtractTextPlugin
-        extract: true,
+        extract: false,
         // css预设器配置项
         loaderOptions: {},
         // 启用 CSS modules for all css / pre-processor files.
