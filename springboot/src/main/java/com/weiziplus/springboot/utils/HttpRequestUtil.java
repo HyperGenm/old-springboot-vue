f33b802eb9f491db2680e16945ea374ca3bf59bf
weiziplus <1602802727@qq.com>
2019-08-05 17:45:31 +0800

    1.1.1.2   修复ip地址为127.0.0.1的bug
    Signed-off-by: weiziplus <1602802727@qq.com>

--- springboot/src/main/java/com/weiziplus/springboot/utils/HttpRequestUtil.java
+++ springboot/src/main/java/com/weiziplus/springboot/utils/HttpRequestUtil.java
@@ -1,11 +1,28 @@
 package com.weiziplus.springboot.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author wanglongwei
  * @data 2019/8/5 8:56
  */
+@Slf4j
 public class HttpRequestUtil {
 
     /**
@@ -46,5 +63,81 @@ public class HttpRequestUtil {
             }
         }
         return ip;
+    }
+
+    public static String post(String url, List<NameValuePair> params) {
+        CloseableHttpClient client = HttpClients.custom().setDefaultRequestConfig(RequestConfig.custom()
+                .setConnectionRequestTimeout(2000).setConnectTimeout(2000).setSocketTimeout(2000).build()).build();
+        try {
+            HttpPost post = new HttpPost(url);
+            if (null != params) {
+                UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(params, StandardCharsets.UTF_8);
+                post.setEntity(urlEncodedFormEntity);
+            }
+            post.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+            CloseableHttpResponse response = client.execute(post);
+            if (HttpServletResponse.SC_OK != response.getStatusLine().getStatusCode()) {
+                log.warn("网络请求出错:url" + url + "---状态码:" + response.getStatusLine().getStatusCode() + "---详情:" + response);
+                return null;
+            }
+            HttpEntity entity = response.getEntity();
+            if (null == entity) {
+                return null;
+            }
+            String result = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+            EntityUtils.consume(entity);
+            return result;
+        } catch (Exception e) {
+            log.warn("请求出错" + e);
+            return null;
+        }
+    }
+
+    public static String post(String url) {
+        return post(url, null);
+    }
+
+    public static String get(String url, Map<String, String> params) {
+        if (null != params) {
+            StringBuilder stringBuilder = new StringBuilder(url);
+            boolean flag = false;
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                String value = entry.getValue();
+                String key = entry.getKey();
+                if (!flag) {
+                    stringBuilder.append("?");
+                    flag = true;
+                } else {
+                    stringBuilder.append("&");
+                }
+                stringBuilder.append(key).append("=").append(value);
+            }
+            url = stringBuilder.toString();
+        }
+        CloseableHttpClient client = HttpClients.custom().setDefaultRequestConfig(RequestConfig.custom()
+                .setConnectionRequestTimeout(2000).setConnectTimeout(2000).setSocketTimeout(2000).build()).build();
+        try {
+            HttpGet get = new HttpGet(url);
+            get.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+            CloseableHttpResponse response = client.execute(get);
+            if (HttpServletResponse.SC_OK != response.getStatusLine().getStatusCode()) {
+                log.warn("网络请求出错:url" + url + "---状态码:" + response.getStatusLine().getStatusCode() + "---详情:" + response);
+                return null;
+            }
+            HttpEntity entity = response.getEntity();
+            if (null == entity) {
+                return null;
+            }
+            String result = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+            EntityUtils.consume(entity);
+            return result;
+        } catch (Exception e) {
+            log.warn("请求出错" + e);
+            return null;
+        }
+    }
+
+    public static String get(String url) {
+        return get(url, null);
     }
 }
