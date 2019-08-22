53e39308b23c22578170f457bc893d2461e9ee12
weiziplus <1602802727@qq.com>
2019-08-22 15:15:34 +0800

    1.1.8.4   新增参数过滤器,过滤sql关键字以及html
    Signed-off-by: weiziplus <1602802727@qq.com>

--- springboot/src/main/java/com/weiziplus/springboot/filter/ParamsFilter.java
+++ springboot/src/main/java/com/weiziplus/springboot/filter/ParamsFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 参数过滤器
@@ -18,6 +20,11 @@ import java.io.IOException;
 public class ParamsFilter implements Filter {
 
     /**
+     * 允许的url---该请求将不会过滤参数
+     */
+    private final List<String> ALLOW_URL = Arrays.asList("", "");
+
+    /**
      * @param servletRequest
      * @param servletResponse
      * @param filterChain
@@ -26,6 +33,14 @@ public class ParamsFilter implements Filter {
      */
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String requestUrl = request.getRequestURI();
+        //放过允许的url
+        if (ALLOW_URL.contains(requestUrl)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        System.out.println(requestUrl);
         filterChain.doFilter(new ParamsHttpServletRequestWrapper((HttpServletRequest) servletRequest), servletResponse);
     }
 
@@ -50,11 +65,15 @@ public class ParamsFilter implements Filter {
             if (null == parameter) {
                 return null;
             }
+            //去除左右的空格
+            parameter = parameter.trim();
+            //替换sql关键字
+            parameter = cleanSqlKeyWords(parameter);
             return HtmlUtils.htmlEscape(parameter);
         }
 
         /**
-         * 去掉字符串左右空格
+         * 过滤参数值
          *
          * @param name
          * @return
@@ -66,12 +85,39 @@ public class ParamsFilter implements Filter {
                 return null;
             }
             for (int i = 0; i < values.length; i++) {
+                //值为空，跳过
                 if (null == values[i]) {
                     continue;
                 }
+                //去除左右的空格
                 values[i] = values[i].trim();
+                //替换sql关键字
+                values[i] = cleanSqlKeyWords(values[i]);
             }
             return values;
+        }
+
+        /**
+         * 不允许的sql关键字
+         */
+        private static final String[] NOT_ALLOW_KEY_WORDS = {"and", "exec", "insert", "select", "delete", "update", "count"
+                , "*", "%", "chr", "mid", "master", "truncate", "char", "declare", ";", "or", "-", "+"};
+
+        /**
+         * 如果传入的值有关键字，并且左右有空格---将sql关键字替换为INVALID
+         *
+         * @param value
+         * @return
+         */
+        private String cleanSqlKeyWords(String value) {
+            String space = " ";
+            for (String keyword : NOT_ALLOW_KEY_WORDS) {
+                if (value.contains(space + keyword) || value.contains(keyword + space)
+                        || value.contains(space + keyword.toUpperCase()) || value.contains(keyword.toUpperCase() + space)) {
+                    value = value.replace(keyword, "INVALID");
+                }
+            }
+            return value;
         }
     }
 }
