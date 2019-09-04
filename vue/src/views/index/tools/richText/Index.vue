794838fd1bddbe82721595d7b1c6f8e7c07205cd
weiziplus <1602802727@qq.com>
2019-09-04 17:59:53 +0800

    1.6.4  富文本输入框
    Signed-off-by: weiziplus <1602802727@qq.com>

---
+++ vue/src/views/index/tools/richText/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="index">
+        <wang-editor :html.sync="html"></wang-editor>
+        <button @click="ceshi">测试</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        components: {
+            'wang-editor': () => import('@/components/wangEditor/Index.vue')
+        },
+        data() {
+            return {
+                html: `<h1>hello wangEditor</h1>`
+            }
+        },
+        methods: {
+            ceshi() {
+                console.log(this.html);
+            }
+        }
+    }
+</script>
