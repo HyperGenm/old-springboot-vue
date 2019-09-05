bf2085dc9e662d0790c02b2e1032afc8ad6726ef
weiziplus <1602802727@qq.com>
2019-09-05 10:44:36 +0800

    1.6.6.5  首页展示
    Signed-off-by: weiziplus <1602802727@qq.com>

--- vue/src/views/index/home/Index.vue
+++ vue/src/views/index/home/Index.vue
@@ -1,15 +1,17 @@
 <template>
     <div id="index">
-        home
+        <el-divider content-position="left">首页展示</el-divider>
+        <el-calendar v-model="date"></el-calendar>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Index"
+        name: "Index",
+        data() {
+            return {
+                date: new Date()
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
