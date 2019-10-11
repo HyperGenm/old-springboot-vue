94ba50cfdac3686ab1cf2a979ee6d4e503bd8c48
WeiziPlus <1602802727@qq.com>
2019-10-11 09:31:49 +0800

    1.9.11  解决线上环境swagger不显示问题
    Signed-off-by: WeiziPlus <1602802727@qq.com>

--- springboot/src/main/java/com/weiziplus/springboot/config/WebConfig.java
+++ springboot/src/main/java/com/weiziplus/springboot/config/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -43,6 +44,20 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public AuthorizationInterceptor authenticationInterceptor() {
         return new AuthorizationInterceptor();
+    }
+
+    /**
+     * 静态资源
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 
     /**
