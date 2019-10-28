a2800a9e35e6c5f6defe19b1c0f99f513d970149
WeiziPlus <1602802727@qq.com>
2019-10-28 19:33:00 +0800

    1.12.1   生成实体类时，将bit类型转为Integer
    Signed-off-by: WeiziPlus <1602802727@qq.com>

--- springboot/src/main/java/org/mybatis/generator/plugin/MyJavaTypeResolver.java
+++ springboot/src/main/java/org/mybatis/generator/plugin/MyJavaTypeResolver.java
@@ -16,8 +16,9 @@ public class MyJavaTypeResolver extends JavaTypeResolverDefaultImpl {
         super.typeMap.put(93, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("TIMESTAMP", new FullyQualifiedJavaType(String.class.getName())));
         //将浮点类型转为Double
         super.typeMap.put(3, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("DECIMAL", new FullyQualifiedJavaType(Double.class.getName())));
-        //将tinyint和smallint转为Integer
-        this.typeMap.put(5, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("SMALLINT", new FullyQualifiedJavaType(Short.class.getName())));
+        //将tinyint和smallint和bit转为Integer
+        this.typeMap.put(5, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("SMALLINT", new FullyQualifiedJavaType(Integer.class.getName())));
         this.typeMap.put(-6, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("TINYINT", new FullyQualifiedJavaType(Integer.class.getName())));
+        this.typeMap.put(-7, new JavaTypeResolverDefaultImpl.JdbcTypeInformation("BIT", new FullyQualifiedJavaType(Integer.class.getName())));
     }
 }
